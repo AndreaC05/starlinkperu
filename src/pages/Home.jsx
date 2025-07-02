6519bd2739278050e7dc2364bf07a1425e7ca64d
AndreaC05 <aguirreandrea457@gmail.com>
2025-07-02 10:18:42 -0500

    avance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import "../style/Home.css";
+import Logo from "../assets/LOGO STARLINK PERÚ.png";
+
 export default function Home() {
     return(
         <>
-         <h1>HOLA</h1>
+         <div className="container_principal_starlink">
+            <div className="banner">
+                <div className="rectangle"></div>
+                 <div className="logo">
+                    {/* <img src={Logo} alt="" /> */}
+                </div>
+                <div className="tit_sub">
+                    <h1>STARLINK</h1>
+                <p>Conectando su empresa al mundo</p>
+                </div>
+            </div>
+            <section className="prioridad_tipo">
+
+            </section>
+         </div>
         </>
     );
 }
