46c4e86c18e151e7134450debae73c89ccff61b6
AndreaC05 <aguirreandrea457@gmail.com>
2025-07-25 11:10:01 -0500

    avance

--- src/components/SITIOFIJOGALERIA/GaleriaImages.jsx
+++ src/components/SITIOFIJOGALERIA/GaleriaImages.jsx
@@ -9,10 +9,10 @@ export default function GaleriaImages({ onClose }) {
 
   // Aquí defines tus URLs de imágenes
   const imageUrls = [
-    "https://res.cloudinary.com/dnao6nouz/image/upload/v1752335339/proyecto_tres_irvym8.jpg",
-    "https://res.cloudinary.com/dnao6nouz/image/upload/v1751571155/Mesa_de_trabajo_2_1_lyziko.jpg",
-    "https://res.cloudinary.com/dnao6nouz/image/upload/v1751571123/Mesa_de_trabajo_1_mjlwmi.jpg",
-    "https://res.cloudinary.com/dnao6nouz/image/upload/v1752335345/proyecto_cuatro_jdnas9.jpg",
+    "https://res.cloudinary.com/dnao6nouz/image/upload/v1753459705/proyecto1_tai5qu.jpg",
+    "https://res.cloudinary.com/dnao6nouz/image/upload/v1753459706/proyecto3_p7ph7s.jpg",
+    "https://res.cloudinary.com/dnao6nouz/image/upload/v1753459705/proyecto4_wlntq3.jpg",
+    "https://res.cloudinary.com/dnao6nouz/image/upload/v1753459706/proyecto2_eafl8x.jpg",
   ];
 
   const responsiveOptions = [
